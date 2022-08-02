53f9e0b27a8fd62a1b402eefb5ff1ea794b09ba5
Austin Gae <austingae@gmail.com>
2022-08-01 20:35:02 -0700

    I got the randomizer to work. I also made a button to randomly pick a character.

--- pages/index.js
+++ pages/index.js
@@ -42,44 +42,22 @@ export default function Home() {
 
 
   function changeCharacterBorderColor() {
-    let randomCharacterNames = [];
+    let TheChosenOne;
 
     const characterNames = [
       'Astra', 'Breach', 'Brimstone', 'Chamber', 'Cypher', 'Jett', 'Kayo', 'Killjoy', 'Neon',
       'Omen', 'Phoenix', 'Raze', 'Reyna', 'Sage', 'Skye', 'Sova', 'Viper', 'Yoru'
     ];
 
-    for (let i = 0; i < 6; i++) {
-      let index = Math.floor(Math.random() * characterNames.length);
-      const randomValorantCharacterName = characterNames[index];
-      randomCharacterNames.push(randomValorantCharacterName);
-      characterNames.splice(index, 1);
-    }
+    let index = Math.floor(Math.random() * characterNames.length);
+    
+    TheChosenOne = characterNames[index];
 
-    console.log(randomCharacterNames);
+    setChosenValorantCharacter(TheChosenOne);
   }
 
+  let [chosenValorantCharacter, setChosenValorantCharacter] = useState("");
 
-  let [valorantCharacters, setValorantCharacters] = useState(
-    [
-      {
-        name: 'Astra',
-        borderColor: 'red',
-      },
-      {
-        name: 'Breach',
-        borderColor: 'red',
-      },
-      {
-        name: 'Brimstone',
-        borderColor: 'red',
-      },
-      {
-        name: 'Chamber',
-        borderColor: 'red',
-      }
-    ]
-  ) 
 
   /*
   let [astra, setAstra] = useState('red');
@@ -131,98 +109,98 @@ export default function Home() {
               placeholder="Select your Map"
               onChange={setValorantMapFunction}
                />
-
-               <button onClick={changeCharacterBorderColor}>Roll your Character</button>
+               
+               <button className={styles.rollYourCharacterButton} onClick={changeCharacterBorderColor}>Roll your Character</button>
             </div>
 
             {/*Right Section: Character Selection & Randomize Button*/}
             <div className={styles.characterSelection}>
-                <div className={styles.characterContainer}>
+                <div className={styles.characterContainer} style={{borderColor: chosenValorantCharacter == "Astra" ? 'gold' : 'red', boxShadow: chosenValorantCharacter == "Astra" ? '-2px 10px 34px 0px rgba(255,215,0,0.75) inset' : ""}}>
                   <img className={styles.characterImage} src={"/images/characters/" + "astra" + ".png"} />
                   <h3 className={styles.characterName}>Astra</h3>
                 </div>
 
-                <div className={styles.characterContainer}>
+                <div className={styles.characterContainer} style={{borderColor: chosenValorantCharacter == "Breach" ? 'gold' : 'red', boxShadow: chosenValorantCharacter == "Breach" ? '-2px 10px 34px 0px rgba(255,215,0,0.75) inset' : ""}}>
                   <img className={styles.characterImage} src={"/images/characters/" + "breach" + ".png"} />
                   <h3 className={styles.characterName}>Breach</h3>
                 </div>
 
-                <div className={styles.characterContainer}>
+                <div className={styles.characterContainer} style={{borderColor: chosenValorantCharacter == "Brimstone" ? 'gold' : 'red', boxShadow: chosenValorantCharacter == "Brimstone" ? '-2px 10px 34px 0px rgba(255,215,0,0.75) inset' : ""}}>
                   <img className={styles.characterImage} src={"/images/characters/" + "brimstone" + ".png"} />
                   <h3 className={styles.characterName}>Brimstone</h3>
                 </div>
 
-                <div className={styles.characterContainer} >
+                <div className={styles.characterContainer}  style={{borderColor: chosenValorantCharacter == "Chamber" ? 'gold' : 'red', boxShadow: chosenValorantCharacter == "Chamber" ? '-2px 10px 34px 0px rgba(255,215,0,0.75) inset' : ""}}>
                   <img className={styles.characterImage} src={"/images/characters/" + "chamber" + ".png"} />
                   <h3 className={styles.characterName}>Chamber</h3>
                 </div>
 
-                <div className={styles.characterContainer} >
+                <div className={styles.characterContainer}  style={{borderColor: chosenValorantCharacter == "Cypher" ? 'gold' : 'red', boxShadow: chosenValorantCharacter == "Cypher" ? '-2px 10px 34px 0px rgba(255,215,0,0.75) inset' : ""}}>
                   <img className={styles.characterImage} src={"/images/characters/" + "cypher" + ".png"} />
                   <h3 className={styles.characterName}>Cypher</h3>
                 </div>
 
-                <div className={styles.characterContainer}>
+                <div className={styles.characterContainer} style={{borderColor: chosenValorantCharacter == "Jett" ? 'gold' : 'red', boxShadow: chosenValorantCharacter == "Jett" ? '-2px 10px 34px 0px rgba(255,215,0,0.75) inset' : ""}}>
                   <img className={styles.characterImage} src={"/images/characters/" + "jett" + ".png"} />
                   <h3 className={styles.characterName}>Jett</h3>
                 </div>
 
-                <div className={styles.characterContainer} >
+                <div className={styles.characterContainer}  style={{borderColor: chosenValorantCharacter == "Kayo" ? 'gold' : 'red', boxShadow: chosenValorantCharacter == "Kayo" ? '-2px 10px 34px 0px rgba(255,215,0,0.75) inset' : ""}}>
                   <img className={styles.characterImage} src={"/images/characters/" + "kayo" + ".png"} />
                   <h3 className={styles.characterName}>KAY/O</h3>
                 </div>
 
-                <div className={styles.characterContainer}>
+                <div className={styles.characterContainer} style={{borderColor: chosenValorantCharacter == "Killjoy" ? 'gold' : 'red', boxShadow: chosenValorantCharacter == "Killjoy" ? '-2px 10px 34px 0px rgba(255,215,0,0.75) inset' : ""}}>
                   <img className={styles.characterImage} src={"/images/characters/" + "killjoy" + ".png"} />
                   <h3 className={styles.characterName}>Killjoy</h3>
                 </div>
 
-                <div className={styles.characterContainer} >
+                <div className={styles.characterContainer}  style={{borderColor: chosenValorantCharacter == "Neon" ? 'gold' : 'red', boxShadow: chosenValorantCharacter == "Neon" ? '-2px 10px 34px 0px rgba(255,215,0,0.75) inset' : ""}}>
                   <img className={styles.characterImage} src={"/images/characters/" + "neon" + ".png"} />
                   <h3 className={styles.characterName}>Neon</h3>
                 </div>
 
-                <div className={styles.characterContainer} >
+                <div className={styles.characterContainer}  style={{borderColor: chosenValorantCharacter == "Omen" ? 'gold' : 'red', boxShadow: chosenValorantCharacter == "Omen" ? '-2px 10px 34px 0px rgba(255,215,0,0.75) inset' : ""}}>
                   <img className={styles.characterImage} src={"/images/characters/" + "omen" + ".png"} />
                   <h3 className={styles.characterName}>Omen</h3>
                 </div>
 
-                <div className={styles.characterContainer}>
+                <div className={styles.characterContainer} style={{borderColor: chosenValorantCharacter == "Phoenix" ? 'gold' : 'red', boxShadow: chosenValorantCharacter == "Phoenix" ? '-2px 10px 34px 0px rgba(255,215,0,0.75) inset' : ""}}>
                   <img className={styles.characterImage} src={"/images/characters/" + "phoenix" + ".png"} />
                   <h3 className={styles.characterName}>Phoenix</h3>
                 </div>
 
-                <div className={styles.characterContainer}>
+                <div className={styles.characterContainer} style={{borderColor: chosenValorantCharacter == "Raze" ? 'gold' : 'red', boxShadow: chosenValorantCharacter == "Raze" ? '-2px 10px 34px 0px rgba(255,215,0,0.75) inset' : ""}}>
                   <img className={styles.characterImage} src={"/images/characters/" + "raze" + ".png"} />
                   <h3 className={styles.characterName}>Raze</h3>
                 </div>
 
-                <div className={styles.characterContainer} >
+                <div className={styles.characterContainer}  style={{borderColor: chosenValorantCharacter == "Reyna" ? 'gold' : 'red', boxShadow: chosenValorantCharacter == "Reyna" ? '-2px 10px 34px 0px rgba(255,215,0,0.75) inset' : ""}}>
                   <img className={styles.characterImage} src={"/images/characters/" + "reyna" + ".png"} />
                   <h3 className={styles.characterName}>Reyna</h3>
                 </div>
 
-                <div className={styles.characterContainer} >
+                <div className={styles.characterContainer}  style={{borderColor: chosenValorantCharacter == "Sage" ? 'gold' : 'red', boxShadow: chosenValorantCharacter == "Sage" ? '-2px 10px 34px 0px rgba(255,215,0,0.75) inset' : ""}}>
                   <img className={styles.characterImage} src={"/images/characters/" + "sage" + ".png"} />
                   <h3 className={styles.characterName}>Sage</h3>
                 </div>
 
-                <div className={styles.characterContainer} >
+                <div className={styles.characterContainer}  style={{borderColor: chosenValorantCharacter == "Skye" ? 'gold' : 'red', boxShadow: chosenValorantCharacter == "Skye" ? '-2px 10px 34px 0px rgba(255,215,0,0.75) inset' : ""}}>
                   <img className={styles.characterImage} src={"/images/characters/" + "skye" + ".png"} />
                   <h3 className={styles.characterName}>Skye</h3>
                 </div>
 
-                <div className={styles.characterContainer}>
+                <div className={styles.characterContainer} style={{borderColor: chosenValorantCharacter == "Sova" ? 'gold' : 'red', boxShadow: chosenValorantCharacter == "Sova" ? '-2px 10px 34px 0px rgba(255,215,0,0.75) inset' : ""}}>
                   <img className={styles.characterImage} src={"/images/characters/" + "sova" + ".png"} />
                   <h3 className={styles.characterName}>Sova</h3>
                 </div>
 
-                <div className={styles.characterContainer}>
+                <div className={styles.characterContainer} style={{borderColor: chosenValorantCharacter == "Viper" ? 'gold' : 'red', boxShadow: chosenValorantCharacter == "Viper" ? '-2px 10px 34px 0px rgba(255,215,0,0.75) inset' : ""}}>
                   <img className={styles.characterImage} src={"/images/characters/" + "viper" + ".png"} />
                   <h3 className={styles.characterName}>Viper</h3>
                 </div>
 
-                <div className={styles.characterContainer}>
+                <div className={styles.characterContainer} style={{borderColor: chosenValorantCharacter == "Yoru" ? 'gold' : 'red', boxShadow: chosenValorantCharacter == "Yoru" ? '-2px 10px 34px 0px rgba(255,215,0,0.75) inset' : ""}}>
                   <img className={styles.characterImage} src={"/images/characters/" + "yoru" + ".png"} />
                   <h3 className={styles.characterName}>Yoru</h3>
                 </div>

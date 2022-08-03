6b6d4943a17dd3a376bc630311fa4e204090704d
Austin Gae <austingae@gmail.com>
2022-08-03 16:07:12 -0700

    I made the How To Use Page responsive

--- pages/how-to-use.js
+++ pages/how-to-use.js
@@ -1,27 +1,31 @@
-import React from 'react'
-
+import React from 'react';
+import styles from '../styles/how-to-use.module.css';
 import { useRouter } from 'next/router';
+
 const HowToUse = () => {
 
   const router = useRouter();
   return (
-    <div style={{color: 'white'}}>
-      <h1>How To Use Valorant Randomizer</h1>
-      <h3>Step 1</h3>
-      <img src='/how-to-use-images-videos/images/character-selection.png' />
-      <h3>Step 2</h3>
-      <img src='/how-to-use-images-videos/images/character-selection-2.png' />
-      <h3>Step 3</h3>
-      <img src='/how-to-use-images-videos/images/character-gun-mission.png' />
-      <h3>That&apos;s About It! Here&apos;s a Quick Demo:</h3>
+    <div className={styles.howToUse}>
+      <h1 className={styles.title}>How To Use Valorant Randomizer</h1>
+      <h3 className={styles.stepTitle}>Step 1</h3>
+      <img className={styles.stepImage} src='/how-to-use-images-videos/images/character-selection.png' />
+      <h3 className={styles.stepTitle}>Step 2</h3>
+      <img className={styles.stepImage} src='/how-to-use-images-videos/images/character-selection-2.png' />
+      <h3 className={styles.stepTitle}>Step 3</h3>
+      <img className={styles.stepImage} src='/how-to-use-images-videos/images/character-gun-mission.png' />
+      <h3 className={styles.stepTitle}>That&apos;s About It! Here&apos;s a Quick Demo:</h3>
       
-      <video style={{width: '800px', height: '800px'}}controls>
+      <video className={styles.demoVideo} controls>
         <source src="/how-to-use-images-videos/video/demo-video.mov" type="video/mov" />
         <source src="/how-to-use-images-videos/video/demo-video.mp4" type="video/mp4" />
         Your browser does not support the demo video.
       </video>
+      
 
-      <button onClick={() => {
+      <h3 className={styles.readyTitle}>Are you Ready?</h3>
+
+      <button className={styles.playButton} onClick={() => {
         router.push('/');
       }}>Play Valorant Randomizer</button>
     </div>

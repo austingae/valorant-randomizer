d75a027f03582128dd87b02b76e03a1ba30d9fc1
Austin Gae <austingae@gmail.com>
2022-08-02 20:40:28 -0700

    When you click the button named New Round New Roll, new gun and mission are displayed. When you click a button named Cant Afford the Gun, new gun will be displayed.

--- pages/characters/[character].js
+++ pages/characters/[character].js
@@ -1,11 +1,41 @@
 import React from 'react'
 
-const Character = ({character}) => {
+import { useState } from 'react'
+
+const Character = ({characterInfoDatum, missions}) => {
+  console.log(missions);
+
+  let guns = ['ares','bucky','bulldog','classic','frenzy','ghost','guardian','judge','marshal','odin','operator','phantom','sheriff','shorty','spectre','stinger','vandal'];
+
+  let [chosenGun, setChosenGun] = useState('');
+  let [chosenMission, setChosenMission] = useState('');
+  
   return (
     <main>
       <div>
-        <img src={"/images/characters/" + character + ".png"} />
-        <h3>{character}</h3>
+        <img src={"/images/characters/" + characterInfoDatum.name.toLowerCase() + ".png"} />
+        <h3>{characterInfoDatum.name}</h3>
+        <p>{characterInfoDatum.role}</p>
+
+        <button onClick={() => {
+          let randomIndex = Math.floor(Math.random() * guns.length);
+          let randomGun = guns[randomIndex];
+          setChosenGun(randomGun);
+
+          let randomIndex2 = Math.floor(Math.random() * missions.length);
+          let randomMission = missions[randomIndex2].mission;
+          setChosenMission(randomMission);
+        }}>New Round, New Roll!</button>
+        
+        <button onClick={() => {
+          let randomIndex = Math.floor(Math.random() * guns.length);
+          let randomGun = guns[randomIndex];
+          setChosenGun(randomGun);
+        }}>Can't Afford the Gun?</button>
+        <h2>Gun</h2>
+        {chosenGun.length > 1 && <img src={"/images/guns/" + chosenGun + ".png"} />}
+        <h2>Mission</h2>
+        <p>{chosenMission}</p>
       </div>
     </main>
 
@@ -32,10 +62,34 @@ export async function getStaticPaths() {
 
 
 export async function getStaticProps(pageContext) {
+
+  //Get Character Info -- name and role and specific missions
   const character = pageContext.params.character;
+
+  let characterInfoJSON = await fetch(`https://raw.githubusercontent.com/austingae/valorant-randomizer/main/public/data/character-info.json`);
+  let characterInfoData = await characterInfoJSON.json();
+
+  let characterInfoDatum = {
+    name: '',
+    role: '',
+  };
+  characterInfoData.forEach((characterInfo) => {
+    if (characterInfo.id == character) {
+      characterInfoDatum.name = characterInfo.name;
+      characterInfoDatum.role = characterInfo.role;
+    }
+  })
+
+  //Get General Missions
+  let missionsInfoJSON = await fetch(`https://raw.githubusercontent.com/austingae/valorant-randomizer/main/public/data/general-info.json`);
+  let missionsInfoData = await missionsInfoJSON.json();
+
+  let missions = missionsInfoData;
+
   return {
     props: {
-      character: character,
+      characterInfoDatum: characterInfoDatum,
+      missions: missions,
     }
   }
 }

851bcc13f4032a81085e531bddaa756af354f0f4
Austin Gae <austingae@gmail.com>
2022-08-04 11:28:39 -0700

    fixing error

--- pages/share-your-ideas.js
+++ pages/share-your-ideas.js
@@ -4,24 +4,24 @@ import styles from '../styles/share-your-ideas.module.css';
 
 import { ShareYourIdeasForm } from '../components/share-your-ideas-form';
 
-import Head from 'next/link';
+import Head from 'next/head';
 const ShareYourIdeas = () => {
   return (
     <>
-    <Head>
-      <title>Share your Ideas</title>
-    </Head>
-    <img className={styles.backgroundImage} src='/share-your-ideas-images/valorant-drawing.png' />
-    <div className={styles.shareYourIdeasContainer}>
-      <div className={styles.shareYourIdeas}>
-        <h1 className={styles.shareYourIdeasTitle}>Share your Ideas!</h1>
-        <h3 className={styles.shareYourIdeasDescription}>Do you have a great idea for a wacky mission? Do you want to provide feedback to improve this randomizer? Do you want to report a bug? </h3>
-        <h3 className={styles.shareYourIdeasDescription}>DuhAppleBaby, the Creator of this Randomizer, would love to hear from you!</h3>
-        <p className={styles.shareYourIdeasDescription2}>Contact DuhAppleBaby at <span className={styles.gmail}>playvalorantrandomizer@gmail.com</span> or fill out the contact form below!</p>
+      <Head>
+        <title>Share your Ideas</title>
+      </Head>
+      <img className={styles.backgroundImage} src='/share-your-ideas-images/valorant-drawing.png' />
+      <div className={styles.shareYourIdeasContainer}>
+        <div className={styles.shareYourIdeas}>
+          <h1 className={styles.shareYourIdeasTitle}>Share your Ideas!</h1>
+          <h3 className={styles.shareYourIdeasDescription}>Do you have a great idea for a wacky mission? Do you want to provide feedback to improve this randomizer? Do you want to report a bug? </h3>
+          <h3 className={styles.shareYourIdeasDescription}>DuhAppleBaby, the Creator of this Randomizer, would love to hear from you!</h3>
+          <p className={styles.shareYourIdeasDescription2}>Contact DuhAppleBaby at <span className={styles.gmail}>playvalorantrandomizer@gmail.com</span> or fill out the contact form below!</p>
 
-        <ShareYourIdeasForm />
+          <ShareYourIdeasForm />
+        </div>
       </div>
-    </div>
     </>
   )
 }
